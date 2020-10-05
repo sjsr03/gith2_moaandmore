9baef6b089f378ea1241d2723f204cef823ae2f0
sojin <sjsr03@hanmail.net>
2020-10-05 11:03:02 +0900

    Goals테이블 관련 DTO, DAO, Service 인터페이스, 구현파일 생성 goals-controller.xml 생성

---
+++ src/goals/model/dao/GoalsDAO.java
@@ -0,0 +1,27 @@
+package goals.model.dao;
+
+import java.util.ArrayList;
+
+import goals.model.dto.GoalsDTO;
+
+public interface GoalsDAO {
+
+	//CRUD
+	
+	//목표 하나가져오기
+	public GoalsDTO selectGoal(int goal_no);
+	
+	//아이디로 목표 전체 가져오기
+	public ArrayList<GoalsDTO> selectAllGoalsById(String id);
+	
+	//삽입
+	public boolean insertGoal(GoalsDTO goal);
+	
+	//수정
+	public boolean modifyGoal(GoalsDTO goal);
+	
+	//삭제
+	public boolean deleteGoal(int goal_no);
+	
+	
+}
---
+++ src/goals/model/dto/GoalsDTO.java
@@ -0,0 +1,88 @@
+package goals.model.dto;
+
+import java.sql.Timestamp;
+
+public class GoalsDTO {
+	private int goal_no;
+	private String id;
+	private String subject;//목표명
+	private int target_money; //목표금액
+	private int saving; //모은돈
+	private Timestamp start_day;// 시작날짜
+	private Timestamp end_day;
+	private char public_ch; //개인0 그룹1
+	private char public_type;// 비공개0 공개0
+	private int team_no;//fk. 그룹목표인 경우 id, 개인인경우 -1
+	
+	public int getGoal_no() {
+		return goal_no;
+	}
+	public void setGoal_no(int goal_no) {
+		this.goal_no = goal_no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getTarget_money() {
+		return target_money;
+	}
+	public void setTarget_money(int target_money) {
+		this.target_money = target_money;
+	}
+	public int getSaving() {
+		return saving;
+	}
+	public void setSaving(int saving) {
+		this.saving = saving;
+	}
+	public Timestamp getStart_day() {
+		return start_day;
+	}
+	public void setStart_day(Timestamp start_day) {
+		this.start_day = start_day;
+	}
+	public Timestamp getEnd_day() {
+		return end_day;
+	}
+	public void setEnd_day(Timestamp end_day) {
+		this.end_day = end_day;
+	}
+	public char getPublic_ch() {
+		return public_ch;
+	}
+	public void setPublic_ch(char public_ch) {
+		this.public_ch = public_ch;
+	}
+	public char getPublic_type() {
+		return public_type;
+	}
+	public void setPublic_type(char public_type) {
+		this.public_type = public_type;
+	}
+	public int getTeam_no() {
+		return team_no;
+	}
+	public void setTeam_no(int team_no) {
+		this.team_no = team_no;
+	}
+	@Override
+	public String toString() {
+		return "GoalsDTO [goal_no=" + goal_no + ", id=" + id + ", subject=" + subject + ", target_money=" + target_money
+				+ ", saving=" + saving + ", start_day=" + start_day + ", end_day=" + end_day + ", public_ch="
+				+ public_ch + ", public_type=" + public_type + ", team_no=" + team_no + "]";
+	}
+	
+	
+	
+
+	
+}
