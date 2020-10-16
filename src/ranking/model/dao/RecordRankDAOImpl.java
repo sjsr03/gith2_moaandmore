e413005d98789a7f3d2f877e27c988d81d91a120
sojin <sjsr03@hanmail.net>
2020-10-16 13:39:08 +0900

    total_rank, record_rank테이블 맵퍼 xml 생성 및 설정

--- src/ranking/model/dao/RecordRankDAOImpl.java
+++ src/ranking/model/dao/RecordRankDAOImpl.java
@@ -1,12 +1,21 @@
 package ranking.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import ranking.model.dto.RecordRankDTO;
 
+
+@Repository
 public class RecordRankDAOImpl implements RecordRankDAO {
 
+	@Autowired
+	SqlSessionTemplate sqlSession =null;
+	
+	
 	@Override
 	public void updateOne(RecordRankDTO dto) {
-		// TODO Auto-generated method stub
 		
 	}
 
--- src/ranking/service/RankingServiceImpl.java
+++ src/ranking/service/RankingServiceImpl.java
@@ -1,15 +1,26 @@
 package ranking.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ranking.model.dao.RecordRankDAOImpl;
 import ranking.model.dto.RecordRankDTO;
 
+
+@Service
 public class RankingServiceImpl implements RankingService{
 	
+	
+	@Autowired
+	RecordRankDAOImpl recordRankDAO = null;
+
 	
 	
 	@Override
 	public void updateRecordRank(RecordRankDTO dto) {
 		
 		
+		recordRankDAO.updateOne(dto);
 		
 	}
 
