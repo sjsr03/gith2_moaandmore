2bc9dad7689a66d05a58fa94417212c566a28216
Jiye Choi <hyde69ciel@gmail.com>
2020-10-21 18:53:12 +0900

    -

--- src/report/controller/bean/ReportBean.java
+++ src/report/controller/bean/ReportBean.java
@@ -18,8 +18,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import budget.model.dao.RecordBudgetDAO;
+import budget.model.dto.BudgetDTO;
+import budget.model.dto.BudgetDetailDTO;
 import budget.model.dto.TotalBudgetDTO;
+import budget.model.dto.TotalBudgetDetailDTO;
 import budget.service.bean.BudgetService;
+import budget.service.bean.RecordService;
+import category.service.bean.CategoryService;
 import report.service.bean.ReportService;
 
 @Controller
@@ -30,6 +36,12 @@ public class ReportBean {
 	private ReportService reportService = null;
 	@Autowired
 	private BudgetService budgetService = null;
+	@Autowired
+	private RecordService recordService = null;
+	@Autowired
+	private CategoryService categoryService = null;
+	@Autowired
+	private RecordBudgetDAO recordBudgetDAO = null;
 	
 	
 	@RequestMapping("report.moa")
@@ -97,5 +109,78 @@ public class ReportBean {
 			return "report/expectation";
 		}
 		
+	}
+	
+	@RequestMapping("recordMacroForm.moa")
+	public String recordMacroForm() {
+		return "report/recordMacro";
+	}
+	
+	@RequestMapping("recordMacro.moa")
+	public String recordMacro(String id, int count, String StartDay, String EndDay, String type) throws SQLException, InterruptedException {
+		
+		List categoryList = null;
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(StartDay);
+		
+		Date startDay = new Date(Timestamp.valueOf(StartDay).getTime());
+		Date endDay = new Date(Timestamp.valueOf(EndDay).getTime());
+		
+		
+		
+		
+		if (type.equals("budget")) {
+			while(startDay.before(endDay)) {
+				
+				Timestamp startTS = Timestamp.valueOf(sdf.format(startDay));
+				// 예산 번호 뽑아오기
+				int budgetNum = budgetService.selectBudgetNum(id, startTS);
+				// 카테고리 번호 뽑아오기
+				categoryList = budgetService.selectBudgetCategoryNums(budgetNum);
+				
+				for (int i = 0; i < count; i++) {
+					BudgetDTO budgetDTO = new BudgetDTO();
+					int random = (int)((Math.random()*(categoryList.size()-1)));
+//					int category_no = ((TotalBudgetDetailDTO)categoryList.get(random)).getCategory_no();
+					int category_no = (int)categoryList.get(random);
+					Timestamp date = Timestamp.valueOf(sdf.format(new Date(startDay.getTime()+((long)(Math.round((1000*60*60*24)*Math.random()))))));
+					budgetDTO.setReg(date);
+					budgetDTO.setAmount((int)(Math.random()*10000) + 100);
+					budgetDTO.setBudget_no(budgetNum);
+					budgetDTO.setCategory_no(category_no);
+					budgetDTO.setId(id);
+					
+
+					
+					// 예산 내역 insert해준 후  구분번호 예산세부내역dto에 다시 세팅해주기 
+					recordBudgetDAO.insertBudget(budgetDTO);
+					int budget_outcome_no = budgetDTO.getBudget_outcome_no();
+					
+					BudgetDetailDTO budgetDetailDTO = new BudgetDetailDTO();
+					budgetDetailDTO.setContent("랜덤" + (int)Math.random()*10);
+					budgetDetailDTO.setBudget_outcome_no(budget_outcome_no);
+					budgetDetailDTO.setImg("");
+					budgetDetailDTO.setMemo("랜덤값");
+					
+					recordBudgetDAO.insertBudgetDetail(budgetDetailDTO);		
+					
+				}
+				
+				startDay.setDate(startDay.getDate()+1);
+			}
+			
+		} else {
+			if (type.equals("income")) {
+				categoryList = categoryService.selectAllIncomeCategoryById(id);
+			}else if (type.equals("outcome")) {
+				categoryList = categoryService.selectAllById(id);
+			}
+			
+			
+			
+		
+		}
+		return "끝";
 	}
 }
